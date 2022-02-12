1ed5403cfa71145a3dbb206bb71e21b2c4379e63
minholeelog <91aiden.lee@gmail.com>
2022-02-13 00:46:00 +0900

    전역 스타일 수정

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as LoaderBar } from './Global/LoaderBar/LoaderBar'
+export { default as SelectBox } from './Global/SelectBox/SelectBox'
 
 export { default as Music } from './Music/Music'
